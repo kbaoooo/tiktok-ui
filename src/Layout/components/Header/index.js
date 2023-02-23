367ca6ed939ad0ee22b0f0281e264d8e1e957a0f
kbao <kbao122003@gmail.com>
2023-02-24 00:01:36 +0700

    build Header UI - 1

--- src/Layout/components/Header/index.js
+++ src/Layout/components/Header/index.js
@@ -1,16 +1,34 @@
 //Library
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmarkCircle, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 // src
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
-const cx = classNames.bind(styles); // 
+const cx = classNames.bind(styles); 
 
 function Header() {
-    return(
+    return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                
+                <div>
+                    <img src={images.logo} alt="logo"/>
+                </div>
+                <div className={cx('search')}>
+                    <input type="text" name="input" placeholder='Search account and videos' spellCheck={false}/>
+                    <button type="button" className={cx('clear-btn')}>
+                        <FontAwesomeIcon icon={faXmarkCircle}/>
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')}/>
+                    <button type="button" className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                    
+                </div>
             </div>
         </header>
     );
